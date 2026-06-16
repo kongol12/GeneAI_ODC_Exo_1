c07b408952711a3cfd0deeadffb29df23f5cdae2
Ghost_FX <kongolmandf@gmail.com>
2026-06-16 13:24:54 +0100

    commit 4:amelioration de commentaire et affichage

--- guess_number.py
+++ guess_number.py
@@ -1,15 +1,17 @@
 import random
-
+#le programme génère un nombre aléatoire entre 1 et 100
 number = random.randint(1, 100)
 
-print("Bienvenue dans mon petit jeu Guess the number")  
+print("Bienvenue dans mon petit jeu Guess the number")
 while True:
     guess = int(input("Devinez le nombre : "))
 
     if guess < number:
-        print("Trop petit")
+        print("Le Nombre est Trop petit")
+
     elif guess > number:
-        print("Trop grand")
+        print("Le Nombre est Trop grand")
     else:
         print("Correct !")
         break
+    #le programme continue à demander à l'utilisateur de deviner jusqu'à ce qu'il trouve le bon nombre
